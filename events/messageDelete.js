ca33b9c2714d7d8929fb5cbda431434df738b471
Yu MORIYA <euro21st@gmail.com>
2023-04-27 17:48:47 +0900

    Add updated_at value to create bookmark

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,12 +4,19 @@ const { logger } = require('../logger');
 module.exports = {
 	name: 'messageDelete',
 	async execute(message) {
+    const interaction = message.interaction;
     if (!message.author || !message.author.username) {
+      if (interaction) {
+        interaction.reply('message author or user was lost, creating ignore was failed.');
+      }
       logger.info('message author or user was lost, creating ignore was failed.');
       return;
     }
     const username = message.author.username;
     await addIgnore(username);
+    if (interaction) {
+      interaction.reply(`added ${username} to ignore list`);
+    }
     logger.info(`update ignores, add: ${username}`);
 	},
 };
