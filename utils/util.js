ea95ccdc0530e9fa116a1083d1cd2bedc6f35693
Yu MORIYA <euro21st@gmail.com>
2024-07-16 19:28:41 +0900

    Modify target message condition

--- utils/util.js
+++ utils/util.js
@@ -3,10 +3,8 @@ const { channelId } = require('../config.json');
 
 // function to judge message is target
 module.exports.isTargetMessage = (message) => {
-  if (!message.member) return false;
-  if (!message.member.roles) return false;
-  if (!message.member.roles.cache) return false;
-  if (!message.member.roles.cache.some(role => role.name === 'IFTTT')) return false;
+  if (!message.author) return false;
+  if (message.author.username !== 'HCW') return false;
   if (!message.channel) return false;
   if (!message.channel.id) return false;
   if (message.channel.id !== channelId) return false;
